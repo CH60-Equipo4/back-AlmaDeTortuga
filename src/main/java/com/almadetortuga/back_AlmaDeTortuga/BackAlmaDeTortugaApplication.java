37622fb7865be0d5f6c537911311eee6f5b8e6a4
zepahua25 <jdaniel.zepahua@gmail.com>
2025-11-19 17:10:46 -0600

    Creacion de Base para SpringBoot
    Se creo el proyecto  con spring Initializer.

---
+++ src/main/java/com/almadetortuga/back_AlmaDeTortuga/BackAlmaDeTortugaApplication.java
@@ -0,0 +1,13 @@
+package com.almadetortuga.back_AlmaDeTortuga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackAlmaDeTortugaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackAlmaDeTortugaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/almadetortuga/back_AlmaDeTortuga/BackAlmaDeTortugaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.almadetortuga.back_AlmaDeTortuga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackAlmaDeTortugaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
