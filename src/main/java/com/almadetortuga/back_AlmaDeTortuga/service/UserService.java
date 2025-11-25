536320cc458a0b4599818fd809e0ad3e63252d38
rosaainz <rosagsainzz@gmail.com>
2025-11-24 23:18:12 -0600

    controller user

--- src/main/java/com/almadetortuga/back_AlmaDeTortuga/service/UserService.java
+++ src/main/java/com/almadetortuga/back_AlmaDeTortuga/service/UserService.java
@@ -1,5 +1,6 @@
 package com.almadetortuga.back_AlmaDeTortuga.service;
 
+import com.almadetortuga.back_AlmaDeTortuga.exceptions.UserNotFoundException;
 import com.almadetortuga.back_AlmaDeTortuga.model.User;
 import com.almadetortuga.back_AlmaDeTortuga.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,8 +27,36 @@ public class UserService {
         return userRepository.save(newUser);
     }
 
+    public User findById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(id));
+    }
+
     public User findByEmail(String email) {
         return userRepository.findByEmail(email);
     }
+
+    // Método para eliminar usuarios id
+    public void deleteUser(Long id) {
+        if (userRepository.existsById(id)) {
+            userRepository.deleteById(id);
+        } else {
+            throw new UserNotFoundException(id);
+        }
+    }
+
+    // Método para actualizar a todo un usuario mediante su id
+    public User updateUser(User user, Long id) {
+        return userRepository.findById(id)
+                .map(userMap -> {
+                    userMap.setName(user.getName());
+                    userMap.setEmail(user.getEmail());
+                    userMap.setPassword(user.getPassword());
+                    return userRepository.save(userMap);
+                })
+                .orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+
 
 }
