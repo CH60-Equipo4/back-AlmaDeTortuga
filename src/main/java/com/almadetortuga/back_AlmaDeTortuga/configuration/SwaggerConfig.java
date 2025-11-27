67d74c73155544ff138fee658ae8b7b69098cbc8
rosaainz <rosagsainzz@gmail.com>
2025-11-26 18:02:45 -0600

    configuration swagger

---
+++ src/main/java/com/almadetortuga/back_AlmaDeTortuga/configuration/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package com.almadetortuga.back_AlmaDeTortuga.configuration;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API ALMA DE TORTUGA - Documentación")
+                        .version("1.0")
+                        .description("API REST para ALMA DE TORTUGA")
+                        .contact(new Contact()
+                                .name("Squirtle Developers")));
+    }
+}
